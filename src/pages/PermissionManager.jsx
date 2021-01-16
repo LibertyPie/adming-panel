ddcd89038fd1e3411594a866b0360caf16047b8f
bornbig <72680443+bornbig@users.noreply.github.com>
2021-01-16 20:34:44 +0530

    Updated UI for Permission Manager

--- src/pages/PermissionManager.jsx
+++ src/pages/PermissionManager.jsx
@@ -17,6 +17,36 @@ class PermissionManager extends Component {
               {/* Breadcrumbs */}
               <BreadCrumb currentPage="Permission Manager" />
               <br />
+
+              <h3>Permission Manager</h3>
+
+              <br />
+              <div className="row justify-content-center">
+                <div className="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+                  <div className="card-2">
+                    <div className="float-right">MODERATOR</div>
+                    <h4>Appoint Admin</h4>
+                    <br />
+                    <br />
+                    <label htmlFor="addr">User Address</label>
+                    <input
+                      type="text"
+                      id="addr"
+                      placeholder="0xf6f574224d28f52FeDE69a68E3Cb63411640E011"
+                    />
+                    <div className="float-right">
+                      <div className="connect-btn small">Save</div>
+                    </div>
+                    <div className="clear"></div>
+
+                    <br />
+                    <br />
+                    <div className="point">
+                      NOTE: You don't have permission to appoint admin
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
