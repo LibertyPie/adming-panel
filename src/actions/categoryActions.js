871bd5a5ad9e935a976af19daf48ca1118ee56b8
bornbig <72680443+bornbig@users.noreply.github.com>
2021-01-23 12:54:45 +0530

    Category Action Updated

--- src/actions/categoryActions.js
+++ src/actions/categoryActions.js
@@ -1,13 +1,20 @@
-import { CATEGORY_LIST_REQUEST, CATEGORY_LIST_ERROR, CATEGORY_LIST_SUCCESS,
-CATEGORY_CU_REQUEST, CATEGORY_CU_ERROR, CATEGORY_CU_SUCCESS,
-CATEGORY_SINGLE_REQUEST,   
-CATEGORY_SINGLE_SUCCESS,
-CATEGORY_SINGLE_ERROR,
-CATEGORY_DELETE_REQUEST,
-CATEGORY_DELETE_SUCCESS,
-CATEGORY_DELETE_ERROR} from "../constants";
+/**
+ * LibertyPie (https://libertypie.com)
+ * @author LibertyPie <hello@libertypie.com>
+ * @license MIT
+ */
+
+import { 
+    CATEGORY_LIST_REQUEST, CATEGORY_LIST_ERROR, CATEGORY_LIST_SUCCESS,
+    CATEGORY_CU_REQUEST, CATEGORY_CU_ERROR, CATEGORY_CU_SUCCESS,
+    CATEGORY_SINGLE_REQUEST, CATEGORY_SINGLE_SUCCESS, CATEGORY_SINGLE_ERROR,
+    CATEGORY_DELETE_REQUEST, CATEGORY_DELETE_SUCCESS, CATEGORY_DELETE_ERROR} from "../constants";
 
 
+/**
+ * get list of categories
+ * @param {contract object} contract
+ */
 export const getCategories = (contract) => async dispatch => {
     dispatch({type: CATEGORY_LIST_REQUEST});
     try{
@@ -20,6 +27,12 @@ export const getCategories = (contract) => async dispatch => {
 }
 
 
+
+/**
+ * get single category
+ * @param {integer} id
+ * @param {contract object} contract
+ */
 export const getSingleCategory = (id, contract) => async dispatch => {
      dispatch({type: CATEGORY_SINGLE_REQUEST});
     try{
@@ -31,6 +44,13 @@ export const getSingleCategory = (id, contract) => async dispatch => {
     }
 }
 
+
+
+/**
+ * create new category
+ * @param {string} name
+ * @param {contract object} contract
+ */
 export const createNewCategory = (name, contract) => async dispatch => {
     dispatch({type: CATEGORY_CU_REQUEST});
     try{
@@ -42,6 +62,14 @@ export const createNewCategory = (name, contract) => async dispatch => {
     }
 }
 
+
+
+/**
+ * update an existing category
+ * @param {integer} id
+ * @param {string} name
+ * @param {contract object} contract
+ */
 export const updateCategory = (id, name, contract) => async dispatch => {
     dispatch({type: CATEGORY_CU_REQUEST});
     try{
@@ -53,6 +81,14 @@ export const updateCategory = (id, name, contract) => async dispatch => {
     }
 }
 
+
+
+
+/**
+ * delete an existing category
+ * @param {integer} id
+ * @param {contract object} contract
+ */
 export const deleteCategory = (id, contract) => async dispatch => {
     dispatch({type: CATEGORY_DELETE_REQUEST});
     try{
