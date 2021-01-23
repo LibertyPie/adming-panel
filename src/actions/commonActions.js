ee4daef9515704d67a9b8170ceda85d0f91a46b1
bornbig <72680443+bornbig@users.noreply.github.com>
2021-01-23 13:17:48 +0530

    Updated Common Action

--- src/actions/commonActions.js
+++ src/actions/commonActions.js
@@ -1,8 +1,23 @@
+/**
+ * LibertyPie (https://libertypie.com)
+ * @author LibertyPie <hello@libertypie.com>
+ * @license MIT
+ */
+
+
+
 import Web3 from "web3";
 import { libertypie_core } from "../abi/libertypie_core";
 import { CONTRACT_ADDRESS, PERMISSION_MANAGER_CONTRACT } from "../config";
 import { WALLET_CONNECT_ERROR, WALLET_CONNECT_REQUEST, WALLET_CONNECT_SUCCESS } from "../constants";
 
+
+/**
+ * connect wallet
+ * takes out account
+ * initialize liberty pie core contract with abi
+ * initialize permission manager contract
+ */
 export const connectWallet = () => async dispatch =>  {
 
     dispatch({type: WALLET_CONNECT_REQUEST});
@@ -21,15 +36,10 @@ export const connectWallet = () => async dispatch =>  {
             await window.ethereum.enable();
             // Acccounts now exposed
           
-            var accounts = await web3.eth.getAccounts();
-            var firstAcc = accounts[0];
-
-            let contract = new web3.eth.Contract(libertypie_core, CONTRACT_ADDRESS);
-
-            let permissionManager = new web3.eth.Contract(libertypie_core, PERMISSION_MANAGER_CONTRACT);
+            let data = await initialize(web3);
           
             hookInWalletChange();
-            dispatch({type: WALLET_CONNECT_SUCCESS, web3: web3, contract: contract, permissionManager: permissionManager, account: firstAcc});
+            dispatch({type: WALLET_CONNECT_SUCCESS, ...data});
         } catch (error) {
             dispatch({type: WALLET_CONNECT_ERROR});
             console.error(error);
@@ -38,40 +48,69 @@ export const connectWallet = () => async dispatch =>  {
         // Use Mist/MetaMask's provider.
         const web3 = window.web3;
   
-        var accounts = await web3.eth.getAccounts();
-        var firstAcc = accounts[0];
+        let data = await initialize(web3);
 
-        let contract = new web3.eth.Contract(libertypie_core, CONTRACT_ADDRESS);
-
-        let permissionManager = new web3.eth.Contract(libertypie_core, PERMISSION_MANAGER_CONTRACT);
         hookInWalletChange();
-        dispatch({type: WALLET_CONNECT_SUCCESS, web3: web3, permissionManager: permissionManager, contract: contract, account: firstAcc});
+        dispatch({type: WALLET_CONNECT_SUCCESS, ...data});
     }else {
         dispatch({type: WALLET_CONNECT_ERROR});
         console.log("Not connected!");
     }
 }
 
+
+
+
+/**
+ * connect if authorize
+ * takes out account
+ * initialize liberty pie core contract with abi
+ * initialize permission manager contract
+ */
 export const connectIfAuthorized = () => async dispatch =>  {
     
     if (window.ethereum) {
         try{
             const web3 = new Web3(window.ethereum);
 
-            var accounts = await web3.eth.getAccounts();
+            let data = await initialize(web3);
 
-            if(accounts.length > 0){
-                var firstAcc = accounts[0];
 
-                let contract = new web3.eth.Contract(libertypie_core, CONTRACT_ADDRESS);
-                hookInWalletChange();
-                dispatch({type: WALLET_CONNECT_SUCCESS, web3: web3, contract: contract, account: firstAcc});
-            }
+            hookInWalletChange();
+            dispatch({type: WALLET_CONNECT_SUCCESS, ...data});
+            
         }catch (error) {
         }
     }
 }
 
+
+
+
+/**
+ * Private Function
+ * hinitialize contract, account & permission manager
+ */
+async function initialize(web3){
+
+    var accounts = await web3.eth.getAccounts();
+
+    if(accounts.length > 0){
+        var firstAcc = accounts[0];
+
+        let contract = new web3.eth.Contract(libertypie_core, CONTRACT_ADDRESS);
+
+        let permissionManager = new web3.eth.Contract(libertypie_core, PERMISSION_MANAGER_CONTRACT);
+
+        return({web3: web3, contract: contract, account: firstAcc, permissionManager: permissionManager});
+    }
+}
+
+
+/**
+ * Private Function
+ * hook with account change and network change
+ */
 function hookInWalletChange(){
     window.ethereum.on('accountsChanged', function (accounts) {
         window.location.href = window.location.href;
